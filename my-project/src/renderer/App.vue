7402da9104e2c9e2a526a855c78fc85b24ce4988
dingyun <992276971@qq.com>
2020-08-10 17:36:05 +0800

    <div id="app"  style="width:2800px">

--- my-project/src/renderer/App.vue
+++ my-project/src/renderer/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app"  style="width:2800px">
     <router-view></router-view>
   </div>
 </template>
--- my-project/src/renderer/components/Landing.vue
+++ my-project/src/renderer/components/Landing.vue
@@ -1,90 +1,76 @@
 <template>
-    <div>
-        <div class="container-sm"></div>
-        <div class="container-md">
+    <div class="container-fiuled">
+        <div class="container-fiuled">
+            <div class="row justify-content-start">
+                <div class="col-4">
+                    One 1
+                </div>
+                <div class="col-4">
+                    One 2
+                </div>
 
-            <div class="row">
+                <div class="col-4">
+                    One 3
+                </div>
+                <div class="col-4">
+                    One 4
+                </div>
+            </div>
+            <div class="row justify-content-end">
+                <div class="col-4">
+                    One 5
+                </div>
+                <div class="col-4">
+                    One 6
+                </div>
+            </div>
+            <div class="row justify-content-around">
+                <div class="col-4">
+                    One 7
+                </div>
+                <div class="col-4">
+                    One 8
+                </div>
+            </div>
+            <div class="row justify-content-between">
+                <div class="col-4">
+                    One 9
+                </div>
+                <div class="col-4">
+                    One 10
+                </div>
+            </div>
+        </div>
+
+        
+        <div class="container-fiuled">
+            <div class="row ">
                 <div class="col-6">col</div>
                 <div class="col-6">col</div>
                 <div class="col-6">col</div>
                 <div class="w-100">ccc</div>
-                <!-- <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div>
-                <div class="col">col</div> -->
                 <div class="col">col</div>
                 <div class="col">col</div>
             </div>
         </div>
 
 
-        <!-- <div class="container-xl">100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide u100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide u100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide u100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide u100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide u100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide u100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide u100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide u100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide u100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide u large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint100% wide until extra large breakpoint</div> -->
 
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="row" style="background-color:lavender>dwaawddw">dwadawdwa </div>
-                <div class="row" style="background-color:lavender>dwaawd"> dwadwqeqwe</div>
-                <div class="row" style="background-color:lavender>dwaawd">hfthfth </div>
-                <div class="row" style="background-color:lavender>dwaawd"> jugughyjghu</div>
-
-
+        <div class="container-fiuled">
+            <div class="row  row-cols-2">
+                <div class="col-sm-6">
+                    <div class="row" style="background-color:lavender>dwaawddw">1 </div>
+                    <div class="row" style="background-color:lavender>dwaawd"> 2</div>
+                    <div class="row" style="background-color:lavender>dwaawd">3 </div>
+                    <div class="row" style="background-color:lavender>dwaawd"> 4</div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="row" style="background-color:lavender>dwaawd">5</div>
+                    <div class="row" style="background-color:lavender>dwaawd">6</div>
+                    <div class="row" style="background-color:lavender>dwaawd">7</div>
+                    <div class="row" style="background-color:lavender>dwaawd">8</div>
+                </div>
             </div>
-            <div class="col-sm-6">
-                <div class="row" style="background-color:lavender>dwaawd">121231233</div>
-                <div class="row" style="background-color:lavender>dwaawd">123123123</div>
-                <div class="row" style="background-color:lavender>dwaawd">4324324</div>
-                <div class="row" style="background-color:lavender>dwaawd">45645645</div>
-            </div>
-
-
-            <!-- <div class="col-xs-6 col-sm-3" style="background-color: #dedef8; 
-            box-shadow: inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </div>
-            <div class="col-xs-6 col-sm-3" style="background-color: #dedef8;box-shadow:         inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                    enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                    nisi ut aliquip ex ea commodo consequat.
-                </p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                    eiusmod tempor incididunt ut.
-                </p>
-            </div>
-
-            <div class="clearfix visible-xs"></div>
-
-            <div class="col-xs-6 col-sm-3" style="background-color: #dedef8; 
-        box-shadow:inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-            </div>
-            <div class="col-xs-6 col-sm-3" style="background-color: #dedef8;box-shadow: 
-        inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                    enim ad minim
-                </p>
-            </div> -->
         </div>
     </div>
 </template>
