872b020b67b084d30e34e561c548e243a3d6553c
ddc168 <992276971@qq.com>
2020-08-10 16:38:57 +0800

    1

---
+++ my-project/src/renderer/components/Landing.vue
@@ -0,0 +1,78 @@
+<template>
+
+
+
+    <!-- <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="row">dwaawd</div>
+                <div class="row">dwaawd</div>
+                <div class="row">dwaawd</div>
+                <div class="row">dwaawd</div>
+
+
+            </div>
+            <div class="col-sm-6">
+                <div class="row">12123123</div>
+                <div class="row">123123123</div>
+                <div class="row">4324324</div>
+                <div class="row">45645645</div>
+            </div>
+
+        </div>
+    </div> -->
+
+
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="row" style="background-color:lavender>dwaawddw">dwadawdwa </div>
+                <div class="row" style="background-color:lavender>dwaawd"> dwadwqeqwe</div>
+                <div class="row" style="background-color:lavender>dwaawd">hfthfth </div>
+                <div class="row" style="background-color:lavender>dwaawd"> jugughyjghu</div>
+
+
+            </div>
+            <div class="col-sm-6">
+                <div class="row" style="background-color:lavender>dwaawd">121231233</div>
+                <div class="row" style="background-color:lavender>dwaawd">123123123</div>
+                <div class="row" style="background-color:lavender>dwaawd">4324324</div>
+                <div class="row" style="background-color:lavender>dwaawd">45645645</div>
+            </div>
+
+
+            <!-- <div class="col-xs-6 col-sm-3" style="background-color: #dedef8; 
+            box-shadow: inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div class="col-xs-6 col-sm-3" style="background-color: #dedef8;box-shadow:         inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                    eiusmod tempor incididunt ut.
+                </p>
+            </div>
+
+            <div class="clearfix visible-xs"></div>
+
+            <div class="col-xs-6 col-sm-3" style="background-color: #dedef8; 
+        box-shadow:inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+            </div>
+            <div class="col-xs-6 col-sm-3" style="background-color: #dedef8;box-shadow: 
+        inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                    enim ad minim
+                </p>
+            </div> -->
+        </div>
+    </div>
+</template>
