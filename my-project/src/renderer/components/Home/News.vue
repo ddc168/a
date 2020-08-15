9176784cbd43a8bbb12324d8d25004b6635da814
ddc168 <992276971@qq.com>
2020-08-15 15:33:58 +0800

    1

--- my-project/src/renderer/components/Home/News.vue
+++ my-project/src/renderer/components/Home/News.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
-        动态
-    </div>
+    <!-- 动态 -->
+  <div>
+    <ul id="example-1">
+      <li v-for="(a, index) in News" :key="index">
+        {{ index+1 }} - {{ a.message }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      News:
+      [
+        { message: '1' },
+        { message: '2' }
+      ]
+    }
+  }
+}
+</script>
