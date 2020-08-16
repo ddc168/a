7829bf57f99d08847c102a9a907db888511bef2e
dingyun <992276971@qq.com>
2020-08-16 11:39:15 +0800

    this.$store.commit('LogIn', this.name, this.password)

--- my-project/src/renderer/components/Home/User.vue
+++ my-project/src/renderer/components/Home/User.vue
@@ -4,34 +4,34 @@
     <form role="form">
       <div class="form-group">
         <label for="name">名称</label>
-        <input type="text" class="form-control" v-model="name" placeholder="请输入名称">
+        <input type="text" class="form-control" v-model='name' placeholder="请输入名称">
       </div>
       <div class="form-group">
         <label for="name">密码</label>
         <input type="text" class="form-control" v-model="password" placeholder="请输入密码">
       </div>
-      <button @click="LogIn">
-        登录
-      </button>
-      <button @click="SignIn">
-        注册
-      </button>
     </form>
+    <button @click="open()">
+      登录
+    </button>
+    <button @click="open()">
+      注册
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
-     return {
+  data () {
+    return {
       name: '',
       password: ''
     }
   },
   methods: {
     open (index) {
-      this.$store.commit('LogIn', {name, password})
-      this.$store.commit('SignIn', {name, password})
+      this.$store.commit('LogIn', this.name, this.password)
+      this.$store.commit('SignIn', this.name, this.password)
       // getNews(this)
     }
   }
