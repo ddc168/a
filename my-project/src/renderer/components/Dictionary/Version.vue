b98a320c6555dcfc363c4d13374e006e4c9a51cb
ddc168 <992276971@qq.com>
2020-08-25 09:47:52 +0800

    1

--- my-project/src/renderer/components/Dictionary/Version.vue
+++ my-project/src/renderer/components/Dictionary/Version.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
-        版本列表
-    </div>
+    <!-- 版本列表 -->
+  <div>
+    <table>
+      <th>版本列表</th>
+      <tr v-for="(version, index) in version" :key="index" @click="open(version)">{{ version }}</tr>
+    </table>
+  </div>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        version: 1
+      }
+    },
+    methods: {
+      open (version) {
+        console.log(version)
+      }
+    }
+  }
+</script>
