39230a3b8fdce1c8851482eef11889764f290927
dingyun <992276971@qq.com>
2020-11-25 09:30:27 +0800

    修改pup

--- puppeteer/server/p2.js
+++ puppeteer/server/p2.js
@@ -1,4 +1,4 @@
-import { P2 } from '../lib/db';
+import { P3 } from '../lib/db';
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
@@ -21,7 +21,9 @@ export  async function p2() {
 
 // 打开搜索结果页面（在主函数里循环执行）
 async function openWeb(page){
-  await page.waitFor(3000)
+  console.log('在浏览器上点击下一页！--15秒后页面被抓取！')
+  console.log('==============================================================================')
+  await page.waitFor(15000)
   let frame = await page.mainFrame()
   let bodyHandle = await frame.$('html');
   let html = await frame.evaluate(body => body.innerHTML, bodyHandle);
@@ -30,30 +32,52 @@ async function openWeb(page){
   openWeb(page)
 }
 
-// 保存清洗后的网页内容到mongodb数据库
+// 保存《知网列表详细页面》清洗后的网页内容到mongodb数据库
 function saveWeb(html){
     const $ = cheerio.load(html)
     const text = $("body").text().replace(/\s*/g,'')
     // console.log(text)
-    let seq, link, name, author, source, date, data, quote, download = ""
+    let seq, link, name, author, source, date, data = ""
     let content = []
-    $("#briefBox #gridTable table tbody tr").each(function(index, element){
+    $("#briefBox #gridTable dl dd").each(function(index, element){
       const item = $(element);
-      seq = item.find(".seq").text();
-      link = item.find(".name a").attr("href")
-      name = item.find(".name a").text()
-      author = item.find(".author a").text();
-      source = item.find(".source a").text();
-      date = item.find(".date").text();
-      data = item.find(".data").text();
-      quote = item.find(".quote").text();
-      download = item.find(".download").text();
+      seq = item.find(".seq").text().replace(/,/g, ";");
+      link = item.find(".middle a").attr("href").replace(/,/g, ";")
+      name = item.find(".middle a").text().replace(/,/g, ";")
+      author = item.find(".middle  .authorinfo").text().replace(/,/g, ";");
+      source = item.find(".middle .baseinfo").text().replace(/,/g, ";");
+      date = item.find(".middle .keywords").text().replace(/,/g, ";");
+      data = item.find(".middle .abstract").text().replace(/,/g, ";");
       const matcht = new RegExp(/^(https?:\/\/)([0-9a-z.]+)(:[0-9]+)?([/0-9a-z.]+)?(\?[0-9a-z&=]+)?(#[0-9-a-z]+)?/i)
       const result = matcht.exec(link)
-      P2.insert({seq:seq, link:link, name:name, author:author, source:source, 
-        date:date, data:data, quote:quote, download:download})
+      P3.insert({seq:seq, link:link, name:name, author:author, source:source, 
+        date:date, data:data})
       console.log(seq)
     })
-
   }
 
+// 保存《知网列表简单页面》清洗后的网页内容到mongodb数据库
+// function saveWeb(html){
+//   const $ = cheerio.load(html)
+//   const text = $("body").text().replace(/\s*/g,'')
+//   console.log(text)
+//   let seq, link, name, author, source, date, data, quote, download = ""
+//   let content = []
+//   $("#briefBox #gridTable table tbody tr").each(function(index, element){
+//     const item = $(element);
+//     seq = item.find(".seq").text();
+//     link = item.find(".name a").attr("href")
+//     name = item.find(".name a").text()
+//     author = item.find(".author a").text();
+//     source = item.find(".source a").text();
+//     date = item.find(".date").text();
+//     data = item.find(".data").text();
+//     quote = item.find(".quote").text();
+//     download = item.find(".download").text();
+//     const matcht = new RegExp(/^(https?:\/\/)([0-9a-z.]+)(:[0-9]+)?([/0-9a-z.]+)?(\?[0-9a-z&=]+)?(#[0-9-a-z]+)?/i)
+//     const result = matcht.exec(link)
+//     // P3.insert({seq:seq, link:link, name:name, author:author, source:source, 
+//     //   date:date, data:data, quote:quote, download:download})
+//     console.log(seq, link, name, author, source, date, data, quote, download)
+//   })
+// }
--- puppeteer/server/p3.js
+++ puppeteer/server/p3.js
@@ -1,4 +1,4 @@
-import { P2 } from '../lib/db';
+import { P3 } from '../lib/db';
 // const pg = require('pg')
 const fs = require("fs")
 const os = require("os")
@@ -6,15 +6,17 @@ const path = require("path")
 const readline = require('line-read')
     
 export function p3(){
-    // P2.find({},{sort: {$seq:1}}).forEach(e => {
-    //     fs.appendFileSync("c:/git/a/puppeteer/p3.txt", e.seq + e.link + e.name + 
-    //     e.author + e.source + e.date + e.data + e.quote + e.download + os.EOL)
+    // P3.find({},{sort: {$seq:1}}).forEach(e => {
+    //     fs.appendFileSync("c:/dockerShare/p3.txt", e.seq + e.link + e.name + 
+    //     e.author + e.source + e.date + e.data + os.EOL)
     // });
+    // + e.quote + e.download
 
-    // f = fs.readFileSync("c:/git/a/puppeteer/p3.txt", 'utf-8')
+
+    // f = fs.readFileSync("c:/dockerShare/p3.txt", 'utf-8')
     
-    // readline.readLineFromFile("c:/git/a/puppeteer/p3.txt").join(function(xs) {
-    //     fs.writeFileSync("c:/git/a/puppeteer/p1.csv", xs)
+    // readline.readLineFromFile("c:/dockerShare/p3.txt").join(function(xs) {
+    //     fs.writeFileSync("c:/dockerShare/p3.csv", xs)
     // });
 }
 
