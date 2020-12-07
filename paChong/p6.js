17730c2ee84515dd88e1be081f0636c1f5ba9e99
dingyun <992276971@qq.com>
2020-12-07 21:30:46 +0800

    js正则

--- paChong/p6.js
+++ paChong/p6.js
@@ -1,12 +1,34 @@
-// 打开拉钩网下载的职位信息，用正则表达式做拆分字段处理
+// 打开拉钩网下载的职位信息，用正则表达式做拆分、查找、替换处理
 const fs = require('fs');
 const os = require('os');
 const f = fs.readFileSync('./docs/zp1.txt').toString().split('\r\n');
-const matcht = new RegExp(/(规模)|(公司主页)|(职位描述)|(工作地址)/);
+const match_1 = new RegExp(/(规模)|(公司主页)|(职位描述)|(工作地址)/);
+const match_2 = new RegExp(/丰台区/)
 let s, r
-for(i in f){
-    s = f[i].replace(/,/g, ";").split(matcht)
-    r = s.filter(n => n)
-    fs.appendFileSync("./docs/zp6.csv", r + os.EOL)
+// 拆分字段split
+// for(i in f){
+//     s = f[i].replace(/,/g, ";").split(match_1)
+//     r = s.filter(n => n)
+//     fs.appendFileSync("./docs/zp6.csv", r + os.EOL)
+// }
+
+// 查找关键词search，返回关键词在文本中的位置数字
+// for(i in f){i
+//     s = f[i].search(/丰台区/)
+//     if(s > 0){
+//         console.log(f[i]+'\n')
+//         console.log(s+'\n')
+//     }
+// }
+
+// 查找关键词match或exec，返回结果列表
+for(i in f){i
+    s = f[i].match(/丰台区/)
+    // s = match_2.exec(f[i])
+    if(s){
+        console.log(s)      
+    }
 }
+// 替换关键词replace
+
 process.exit()
--- paChong/python/p1.py
+++ paChong/python/p1.py
@@ -20,15 +20,19 @@ del df_1['公司主页']
 del df_1['职位描述']
 del df_1['工作地址']
 
+# 去除重复记录
+df_1.drop_duplicates(keep='first', inplace=True)
+
 # 查找字段中包含关键词
 df_2 = df_1.loc[(df_1['dz'].str.contains('朝阳区'))]
 # 使用正则表达式
-df_2 = df_1.loc[(df_1['dz'].str.contains('朝阳区 | 海淀区'))]
+df_2 = df_1.loc[(df_1['dz'].str.contains('房山区|丰台区'))]
 # 职位要求中包含“实习 | 学习”
-df_2 = df_1.loc[(df_1['zw'].str.contains('实习 | 学习'))]
+df_2 = df_1.loc[(df_1['zw'].str.contains('实习|学习'))]
 # 岗位要求中包含“数据”
-df_2 = df_1.loc[(df_1['gw'].str.contains('数据'))]
-df_3 = df_2.loc[(df_2['gw'].str.contains('服装'))]
+df_2 = df_1.loc[(df_1['gw'].str.contains('数据|游戏'))]
+df_3 = df_1.loc[(df_1['gw'].str.contains('服装')) & (df_1['gw'].str.contains('游戏'))]
 
 # 把处理结果写入csv文件
-df_3.to_csv('C:/dockerShare/git/a/paChong/docs/zpp6.csv')
+df_2['gw'].to_csv('C:/dockerShare/git/a/paChong/docs/zpp7.csv')
+
